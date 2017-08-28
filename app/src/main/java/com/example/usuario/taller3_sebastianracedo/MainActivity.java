e21d4cdcf0b60dddb36b30b45c16d36a3349093c
JoaoRacedo <racedo@uninorte.edu.co>
2017-08-28 17:33:01 -0500

    first commit

---
+++ app/src/main/java/com/example/usuario/taller3_sebastianracedo/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.usuario.taller3_sebastianracedo;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final LinearLayout MLayout = (LinearLayout) findViewById(R.id.Mlayout);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            counter ++;
+            View Campo = getLayoutInflater().inflate(R.layout.content_main,null);
+            TextView Label = (TextView)  Campo.findViewById(R.id.textView);
+            Button button = (Button) Campo.findViewById(R.id.button);
+
+            Label.setText("Campo " + counter);
+            button.setTag(counter);
+
+            button.setOnClickListener(new View.OnClickListener(){
+                                          @Override
+                public void onClick(View view){
+                                              Log.d("TAG","Campo :" + view.getTag());
+                                          }
+            }
+            );
+                MLayout.addView(Campo);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
